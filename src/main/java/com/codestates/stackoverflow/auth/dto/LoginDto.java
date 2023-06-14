4957c8749d3f38ed05b42c16115b628fede0db01
jeein2222 <jiinbag057@gmail.com>
2023-06-14 17:07:26 +0900

    feat: 회원 CRUD, jwt 인증 인가 구현

---
+++ src/main/java/com/codestates/stackoverflow/auth/dto/LoginDto.java
@@ -0,0 +1,10 @@
+package com.codestates.stackoverflow.auth.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LoginDto {
+    private String username;
+    private String password;
+
+}
---
+++ src/main/java/com/codestates/stackoverflow/member/service/MemberService.java
@@ -0,0 +1,131 @@
+package com.codestates.stackoverflow.member.service;
+
+import com.codestates.stackoverflow.auth.utils.CustomAuthorityUtils;
+import com.codestates.stackoverflow.exception.BusinessLogicException;
+import com.codestates.stackoverflow.exception.ExceptionCode;
+import com.codestates.stackoverflow.member.entity.Member;
+import com.codestates.stackoverflow.member.repository.MemberRepository;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ *  - 메서드 구현
+ *  - DI 적용
+ *  - Spring Data JPA 적용
+ *  - 트랜잭션 적용
+ */
+@Transactional
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final ApplicationEventPublisher publisher;
+
+    // 추가
+    private final PasswordEncoder passwordEncoder;
+    private final CustomAuthorityUtils authorityUtils;
+
+    public MemberService(MemberRepository memberRepository,
+                         ApplicationEventPublisher publisher,
+                         PasswordEncoder passwordEncoder,
+                         CustomAuthorityUtils authorityUtils) {
+        this.memberRepository = memberRepository;
+        this.publisher = publisher;
+        this.passwordEncoder = passwordEncoder;
+        this.authorityUtils = authorityUtils;
+    }
+
+    public Member createMember(Member member) {
+        verifyExistsEmail(member.getEmail());
+
+        // 추가: Password 암호화
+        String encryptedPassword = passwordEncoder.encode(member.getPassword());
+        member.setPassword(encryptedPassword);
+
+        // 추가: DB에 User Role 저장
+        List<String> roles = authorityUtils.createRoles(member.getEmail());
+        member.setRoles(roles);
+
+        Member savedMember = memberRepository.save(member);
+
+
+        //publisher.publishEvent(new MemberRegistrationApplicationEvent(savedMember));
+        return savedMember;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
+    public Member updateMember(Member member) {
+        Member findMember = findVerifiedMember(member.getMemberId());
+
+        Optional.ofNullable(member.getName())
+                .ifPresent(name -> findMember.setName(name));
+
+        Optional.ofNullable(passwordEncoder.encode(member.getPassword()))
+                .ifPresent(password -> findMember.setPassword(password));
+
+        //System.out.println("new password:"+findMember.getPassword());
+
+        return memberRepository.save(findMember);
+    }
+
+    @Transactional(readOnly = true)
+    public Member findMember(long memberId) {
+        return findVerifiedMember(memberId);
+    }
+
+    @Transactional(readOnly = true)
+    public Member findMember(String email){
+        return findVerifiedMember(email);
+    }
+
+    public Page<Member> findMembers(int page, int size) {
+        return memberRepository.findAll(PageRequest.of(page, size,
+                Sort.by("memberId").descending()));
+    }
+
+    public void deleteMember(long memberId) {
+        Member findMember = findVerifiedMember(memberId);
+
+        memberRepository.delete(findMember);
+    }
+
+    public void deleteAll() {
+        memberRepository.deleteAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Member findVerifiedMember(long memberId) {
+        Optional<Member> optionalMember =
+                memberRepository.findById(memberId);
+        Member findMember =
+                optionalMember.orElseThrow(() ->
+                        new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+        return findMember;
+    }
+
+    public Member findVerifiedMember(String email){
+        Optional<Member> optionalMember =
+                memberRepository.findByEmail(email);
+        Member findMember =
+                optionalMember.orElseThrow(() ->
+                        new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+        return findMember;
+    }
+
+    private void verifyExistsEmail(String email) {
+        Optional<Member> member = memberRepository.findByEmail(email);
+        if (member.isPresent())
+            throw new BusinessLogicException(ExceptionCode.MEMBER_EXISTS);
+    }
+
+
+}
