03e8824f328b9b289c7493611b695246cd00334a
qpwoei0123 <qpwoei0123@naver.com>
2023-06-15 18:18:59 +0900

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,11 @@ import Nav from './component/Nav'
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-        <Nav/>
-        <Footer/>
-=======
 
-      
       <Nav/>
       <SideBar/>
       <Footer/>
 
->>>>>>> 6dd7b254105322cf2133e06005005c99b6997c8b
     </div>
   )
 }
