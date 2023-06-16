f83c694900ee1204460a82541b22ff5a26d03aa5
qpwoei0123 <qpwoei0123@naver.com>
2023-06-16 13:20:50 +0900

    리덕스스토어_기본설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import SideBar from "./component/SideBar";
 import Footer from './component/Footer'
 import Nav from './component/Nav'
-
+import store from "./store/store";
+import { Provider } from "react-redux";
 
 function App() {
+
   return (
     <div>
-
-      <Nav/>
-      <SideBar/>
-      <Footer/>
-
+      <Provider store={store}>
+        <Nav/>
+        <SideBar/>
+        <Footer/>
+      </Provider>
     </div>
   )
 }
--- client/src/store/memberSlice.js
+++ client/src/store/memberSlice.js
@@ -1,15 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+    data :  {
+        memberId : 1,
+        email : "jeein1@gmail.com",
+        name : "Jeein Park1"
+      }
+}
+
 const memberSlice = createSlice({
     name: 'member',
-    initialState: null,
+    initialState,
     reducers: {
-      setMember: (state, action) => {
-        return action.payload;
+      setName: (state, action) => {
+        const newnName = action.payload;
+        state.data.name = newnName;
       },
     }
   });
 
 export default memberSlice;
-export const { setMember } = memberSlice.actions;
+export const { setName } = memberSlice.actions;
   
