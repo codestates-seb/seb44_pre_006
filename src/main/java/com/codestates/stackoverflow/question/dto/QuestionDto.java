3d98ccfd46eb69f0c13778b4af3a1e6feff4369d
kimeojin <kimaj2001@gmail.com>
2023-06-18 18:40:54 +0900

    feat - RequestDto title Max(150) 설정

--- src/main/java/com/codestates/stackoverflow/question/dto/QuestionDto.java
+++ src/main/java/com/codestates/stackoverflow/question/dto/QuestionDto.java
@@ -2,6 +2,7 @@ package com.codestates.stackoverflow.question.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 
 public class QuestionDto {
@@ -12,6 +13,7 @@ public class QuestionDto {
     @AllArgsConstructor
     public static class PostRequest {
         @NotBlank(message = "제목을 입력해야 합니다.")
+        @Max(value = 150, message = "제목은 최대 150자까지 입력할 수 있습니다.")
         private String title;
         @NotBlank(message = "본문을 작성해야 합니다.")
         private String content;
