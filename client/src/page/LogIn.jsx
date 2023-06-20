b331bdac4e1c952cd52624f6c5cfd906e58ceefd
qpwoei0123 <qpwoei0123@naver.com>
2023-06-20 13:38:38 +0900

    로그인 페이지 구조 완

--- client/src/page/LogIn.jsx
+++ client/src/page/LogIn.jsx
@@ -1,10 +1,24 @@
 import Login from "../component/Login";
+import SideBar from "../component/SideBar";
+import { styled } from "styled-components";
 
+const LoginContainer = styled.div`
+  display: flex;
+`
+const LoginBox = styled.div`
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 function LogIn() {
   return (
-    <div>
-      <Login/> <Login/>
-    </div>
+    <LoginContainer className="LoginContainer">
+      <SideBar/>
+      <LoginBox className="LoginBox">
+        <Login/>
+      </LoginBox>
+    </LoginContainer>
   );
 }
 
