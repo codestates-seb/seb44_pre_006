85af5a5ec127bf26e50fdf02261047314d3e8783
qpwoei0123 <qpwoei0123@naver.com>
2023-06-21 23:14:01 +0900

    회원가입 구현

--- client/src/component/SighUp.js
+++ client/src/component/SighUp.js
@@ -1,35 +1,53 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import GoogleButton from "./GoogleButton";
-import { 
-    LoginContainer as SighUpContaner, 
-    LoginForm as SighUpForm, 
-    LoginInputForm as SighUpInPutForm} from "./Login";
+import { LoginContainer as SignUpContainer, LoginForm as SignUpForm, LoginInputForm as SignUpInputForm } from "./Login";
+import { useRef } from 'react';
+import axios from "axios";
 
 function SignUp() {
-    return(
-        <SighUpContaner>
-            <GoogleButton/>
-            <SighUpForm>
-                <SighUpInPutForm className="LoginInputForm">
-                <h3>Display name</h3>
-                <input/>
-                </SighUpInPutForm>
-                <SighUpInPutForm className="LoginInputForm">
-                <h3>Email</h3>
-                <input/>
-                </SighUpInPutForm>
-                <SighUpInPutForm className="LoginInputForm">
-                <h3>Password</h3>
-                <input/>
-                </SighUpInPutForm>
-                <button>Sigh Up</button>
-            </SighUpForm>
-            <p>
-            Already have an account? &nbsp; 
-            <Link to='/users/login'>Log in</Link>
-        </p>
-        </SighUpContaner>
-    )
+    const navigate = useNavigate();
+    const nameRef = useRef(null);
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
+    const onSignUpHandler = async () => {
+    const name = nameRef.current.value;
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    await axios
+      .post('/users/signup', { email:email, password:password, name:name })
+      .then((response) => {
+        console.log(response);
+        navigate('/users/login');
+      })
+      .catch((err) =>{ console.log(err)});
+    // navigate()를 사용하여 다른 페이지로 이동 가능
+  };
+
+  return (
+    <SignUpContainer>
+      <GoogleButton />
+      <SignUpForm>
+        <SignUpInputForm className="LoginInputForm">
+          <h3>Display name</h3>
+          <input ref={nameRef} id="nameRef" />
+        </SignUpInputForm>
+        <SignUpInputForm className="LoginInputForm">
+          <h3>Email</h3>
+          <input ref={emailRef} id="emailRef" />
+        </SignUpInputForm>
+        <SignUpInputForm className="LoginInputForm">
+          <h3>Password</h3>
+          <input ref={passwordRef} id="passwordRef" />
+        </SignUpInputForm>
+        <button onClick={onSignUpHandler}>Sign Up</button>
+      </SignUpForm>
+      <p>
+        Already have an account? <Link to="/users/login">Log in</Link>
+      </p>
+    </SignUpContainer>
+  );
 }
 
 export default SignUp;
