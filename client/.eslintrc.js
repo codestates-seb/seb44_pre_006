3c5790e52c4bbf52a15957ecaca16db3dc9930f7
qpwoei0123 <qpwoei0123@naver.com>
2023-06-22 23:59:27 +0900

    린터 이동

---
+++ client/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
