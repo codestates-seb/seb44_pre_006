4aee5e27a2f564ec6f1b9469f736fb19cc5578e5
qpwoei0123 <qpwoei0123@naver.com>
2023-06-23 18:45:41 +0900

    검색 api제외 완

--- client/src/component/TextEditor.js
+++ client/src/component/TextEditor.js
@@ -1,41 +1,41 @@
-// import { styled } from 'styled-components';
-// import { Editor } from '@toast-ui/react-editor';
-// import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
-// import '@toast-ui/editor/dist/toastui-editor.css';
-// import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
+import { styled } from 'styled-components';
+import { Editor } from '@toast-ui/react-editor';
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
+import '@toast-ui/editor/dist/toastui-editor.css';
+import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
 
-// const TextEditor = styled.div`
-//   border-radius: 4px;
-//   border: 1px solid var(--black-200);
-//   box-shadow: none;
-//   position: relative;
-// `;
+const TextEditor = styled.div`
+  border-radius: 4px;
+  border: 1px solid var(--black-200);
+  box-shadow: none;
+  position: relative;
+`;
 
-// function EditorBox() {
-//   const toolbarItems = [
-//     ['heading', 'bold', 'italic', 'strike'],
-//     ['hr'],
-//     ['ul', 'ol', 'task'],
-//     ['table', 'link'],
-//     ['image'],
-//     ['code'],
-//     ['scrollSync'],
-//   ];
-//   return (
-//     <div>
-//       <TextEditor>
-//         <Editor
-//           initialValue=""
-//           previewStyle="vertical"
-//           initialEditType="wysiwyg"
-//           useCommandShortcut={false}
-//           plugins={[colorSyntax]}
-//           hideModeSwitch={true}
-//           toolbarItems={toolbarItems}
-//         />
-//       </TextEditor>
-//     </div>
-//   );
-// }
+function EditorBox() {
+  const toolbarItems = [
+    ['heading', 'bold', 'italic', 'strike'],
+    ['hr'],
+    ['ul', 'ol', 'task'],
+    ['table', 'link'],
+    ['image'],
+    ['code'],
+    ['scrollSync'],
+  ];
+  return (
+    <div>
+      <TextEditor>
+        <Editor
+          initialValue=""
+          previewStyle="vertical"
+          initialEditType="wysiwyg"
+          useCommandShortcut={false}
+          plugins={[colorSyntax]}
+          hideModeSwitch={true}
+          toolbarItems={toolbarItems}
+        />
+      </TextEditor>
+    </div>
+  );
+}
 
-// export default EditorBox;
+export default EditorBox;
