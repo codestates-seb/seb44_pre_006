446f4b54c86580cc1f51e41e2564900295cf04e7
qpwoei0123 <qpwoei0123@naver.com>
2023-06-26 12:08:34 +0900

    필터 빼고 끝

---
+++ client/src/api/getUserQnA.js
@@ -0,0 +1,25 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+//1.5 회원 질문, 답변 정보 조회
+const URL = process.env.REACT_APP_EC2_URL;
+
+const fetchGetUserQnA = createAsyncThunk('user/fetchGetUserQnA', async (memberId) => {
+  const url = `${URL}/users/getInfo/${memberId}`;
+  const token = localStorage.getItem('jwtToken');
+
+  const response = await axios.get(url, {
+    headers: {
+      Authorization: token
+    },
+  })
+  
+  try {
+    if (response.status >= 200 && response.status < 300)
+    return response.data; 
+  } 
+  catch (error) {
+    return error;
+  }
+});
+
+export default fetchGetUserQnA
--- client/src/page/Profile.jsx
+++ client/src/page/Profile.jsx
@@ -3,32 +3,36 @@ import ProfileHeader from "../component/user/ProfileHeader"
 import SideBar from "../component/SideBar";
 import ProfileCartegory from "../component/user/ProfileCategory"
 import {DataFilterWrapper, NewestButton, UnansweredButton} from "./AllQuestion"
+import fetchGetUserQnA from "../api/getUserQnA"
+import { useEffect , useState} from "react";
+import { useDispatch } from "react-redux";
+import displayCreatedAt from '../utils/displayCreateAt';
+import { useNavigate } from "react-router-dom";
 
 const ProfilePageContainer = styled.div`
   display: flex;
 `
 const ProfileContainer = styled.div`
-  width: 100%;
-  margin: 30px;
+  width: 75%;
+  margin: 2%;
 `
 const StatsContainer = styled.section`
-  width: 600px;
-  margin-left: 20px;
+  width: 30%;
+  margin-left: 2%;
   > h2 {
   }
   > .StatsBox{
     border: 2px solid var(--silver);
     border-radius: 20px;
-    height: 200px;
-    width: 100%;
+    height: 100%;
     display: flex;
     flex-direction: column;
     > .column {
       display: flex;
       flex-grow: 1;
       > .stat {
-        margin: 0 30px;
-        margin-right: 180px;
+        margin: 0 5%;
+        width: 40%;
         & > :first-child {
           font-size: 130%;
           color: var(--black);
@@ -45,9 +49,9 @@ const FlexDiv = styled.div`
 `
 
 const ActivityContainer = styled.section`
-  margin-left: 20px;  
+  margin-left: 2%;  
   height: 400px;
-  width: 500px;
+  width: 48%;
   display: flex;
   flex-direction: column;
   > .ActivityHeader{
@@ -98,6 +102,17 @@ const ActivityContainer = styled.section`
 `
 
 function Profile() {
+  const dispatch = useDispatch();
+  const path = window.location.pathname;
+  const memberId = path.slice(path.lastIndexOf("/") + 1);
+  const [QnA, setQnA] = useState(null)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    dispatch(fetchGetUserQnA(memberId)).then(data => setQnA(data.payload.data));
+    console.log(QnA)
+  },[dispatch])
+
   return (
     <ProfilePageContainer>
       <SideBar/>
@@ -106,28 +121,22 @@ function Profile() {
         <ProfileCartegory text={`Profile`}/>
         <StatsContainer>
           <h2>Stats</h2>
-          <div className="StatsBox">
+            {QnA
+            ? (  <div className="StatsBox">
             <div className="column">
               <div className="stat">
-                <p>a</p>
+                <p>{QnA.questions.length}</p>
                 <p>questions</p>
               </div>
               <div className="stat">
-                <p>a</p>
+              <p>{QnA.answers.length}</p>
                 <p>answers</p>
               </div>
-            </div>
-            <div className="column">
-              <div className="stat">
-                <p>a</p>
-                <p>createdAt</p>
-              </div>
-              <div className="stat">
-                <p>a</p>
-                <p>email</p>
-              </div>
-            </div>
-          </div>
+             </div>
+            </div>)
+            : null}
+          
+          
         </StatsContainer>
         <ProfileCartegory text={`Activity`}/>
         <FlexDiv>
@@ -144,11 +153,15 @@ function Profile() {
             </DataFilterWrapper>
           </div>
           <div className="ActivityBox">
-            <div className="ActivityItem">
-              <p className="view">View</p>
-              <p className="title">titleasdasdasdsadasdasdasdadsadasdasdasdasdasd</p>
-              <p className="createdAt">createdAt</p>
-            </div>
+          {QnA ? (
+            QnA.questions.slice(0, 5).map(el => (
+              <div className="ActivityItem">
+                <p className="view">{el.viewCount}</p>
+                <p className="title" onClick={()=> navigate(`/question/${el.id}`)}>{el.title}</p>
+                <p className="createdAt">{displayCreatedAt(el.createdAt)}</p>
+              </div>
+            ))
+          ) : null}
           </div>
         </ActivityContainer>
         <ActivityContainer>
@@ -164,11 +177,15 @@ function Profile() {
             </DataFilterWrapper>
           </div>
           <div className="ActivityBox">
-            <div className="ActivityItem">
-              <p className="view">View</p>
-              <p className="title">titleasdasdasdsadasdasdasdadsadasdasdasdasdasd</p>
-              <p className="createdAt">createdAt</p>
-            </div>
+          {QnA ? (
+            QnA.answers.slice(0, 5).map(el => (
+              <div className="ActivityItem">
+                <p className="view">0</p>
+                <p className="title">{el.content}</p>
+                <p className="createdAt">{displayCreatedAt(el.createdAt)}</p>
+              </div>
+            ))
+          ) : null}
           </div>
         </ActivityContainer>
         </FlexDiv>
